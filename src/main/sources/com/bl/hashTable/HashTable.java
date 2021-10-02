24222e1f65c57b419a3554e81749566332d06102
Aman <you@example.com>
2021-10-02 20:29:14 +0530

    Frequency Of word in paragarph

--- src/main/sources/com/bl/hashTable/HashTable.java
+++ src/main/sources/com/bl/hashTable/HashTable.java
@@ -1,31 +1,32 @@
-	package com.bl.hashTable;
+package com.bl.hashTable;
 	
 	/**
-	 * Purpose  - To add values in Binary Search Tree and check whether 63 is present.
+	 * Purpose  - A * Purpose  - Ability to find frequency of words in large paragraph
 	 * @author  - JaswinderSingh
 	 * @version - 16.0
 	 * @since   - 30-09-2021
 	 */
 	
 	public class HashTable {
-	
+
 	    public static void main(String[] args) {
-	        HashTableImplementation<String, Integer> hashTable = new HashTableImplementation<String, Integer>();
-	        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-	
+	        HashTableImplementation<String, Integer> hashImpl = new HashTableImplementation<String, Integer>();
+	        String message = "To be or not to be";
+
 	        // Split the words when a white space comes up and Convert all the words in lowercase
-	        String[] paragraphArray =	paragraph.toLowerCase().split(" ");
-	
-	        for (String word : paragraphArray) {
+	        String[] messageArray = message.toLowerCase().split(" ");
+
+	        for (String word : messageArray) {
 	            // Search for the word in the linked list
-	            Integer value = hashTable.get(word);
+	            Integer value = hashImpl.get(word);
 	            // If not found, frequency of the word StayWith 1
 	            if (value == null)
 	                value = 1;
-	            else   // If found, frequency of the word increases by 1
-	            	value = value + 1;
-	            hashTable.add(word, value);
+	            else
+	                // If found, frequency of the word increases by 1+
+	                value = value + 1;
+	            hashImpl.add(word, value);
 	        }
-	        System.out.println(hashTable);
+	        System.out.println(hashImpl);
 	    }
 	}
