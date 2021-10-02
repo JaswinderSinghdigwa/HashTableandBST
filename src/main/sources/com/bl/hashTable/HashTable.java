c7bd0976d3cbb8cceb7fb5d44a0a146837bb16f5
Aman <you@example.com>
2021-10-02 19:49:42 +0530

    Frequency Of Paragraph

--- src/main/sources/com/bl/hashTable/HashTable.java
+++ src/main/sources/com/bl/hashTable/HashTable.java
@@ -1,31 +1,31 @@
-package com.bl.hashTable;
-
-/**
- * Purpose  - To add values in Binary Search Tree and check whether 63 is present.
- * @author  - JaswinderSingh
- * @version - 16.0
- * @since   - 30-09-2021
- */
-
-public class HashTable {
-
-    public static void main(String[] args) {
-        HashTableImplementation<String, Integer> hashTable = new HashTableImplementation<String, Integer>();
-        String message = "To be or not to be";
-
-        // Split the words when a white space comes up and Convert all the words in lowercase
-        String[] messageArray = message.toLowerCase().split(" ");
-
-        for (String word : messageArray) {
-            // Search for the word in the linked list
-            Integer value = hashTable.get(word);
-            // If not found, frequency of the word StayWith 1
-            if (value == null)
-                value = 1;
-            else   // If found, frequency of the word increases by 1
-            	value = value + 1;
-            hashTable.add(word, value);
-        }
-        System.out.println(hashTable);
-    }
-}
+	package com.bl.hashTable;
+	
+	/**
+	 * Purpose  - To add values in Binary Search Tree and check whether 63 is present.
+	 * @author  - JaswinderSingh
+	 * @version - 16.0
+	 * @since   - 30-09-2021
+	 */
+	
+	public class HashTable {
+	
+	    public static void main(String[] args) {
+	        HashTableImplementation<String, Integer> hashTable = new HashTableImplementation<String, Integer>();
+	        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+	
+	        // Split the words when a white space comes up and Convert all the words in lowercase
+	        String[] paragraphArray =	paragraph.toLowerCase().split(" ");
+	
+	        for (String word : paragraphArray) {
+	            // Search for the word in the linked list
+	            Integer value = hashTable.get(word);
+	            // If not found, frequency of the word StayWith 1
+	            if (value == null)
+	                value = 1;
+	            else   // If found, frequency of the word increases by 1
+	            	value = value + 1;
+	            hashTable.add(word, value);
+	        }
+	        System.out.println(hashTable);
+	    }
+	}
--- src/main/sources/com/bl/hashTable/HashTableImplementation.java
+++ src/main/sources/com/bl/hashTable/HashTableImplementation.java
@@ -1,61 +1,85 @@
-package com.bl.hashTable;
-
-
-
-public class HashTableImplementation<K, V> {
-    Node head;
-    Node tail;
-
-    public void add(K key, V value) {
-        Node<K, V> myNode = searchNode(key);
-        if (myNode == null) {
-            myNode = new Node<>(key, value);
-            this.append(myNode);
-        } else {
-            myNode.setValue(value);
-        }
-    }
-
-    // Append the value in the linked list
-    public void append(Node myNode) {
-        if (this.head == null)
-            this.head = myNode;
-        if (this.tail == null)
-            this.tail = myNode;
-        else {
-            this.tail.setNext(myNode);
-            this.tail = myNode;
-        }
-    }
-
-    // Searching for the word in the linked list
-    public Node<K, V> searchNode(K data) {
-        Node currentNode = head;
-        int position = 0;
-        while (currentNode != null) {
-            position++;
-            if (currentNode.getKey().equals(data)) {
-                return currentNode;
-            }
-            currentNode = currentNode.getNext();
-        }
-        return currentNode;
-    }
-
-    // Searching for the word and get the value from the linked list
-    public V get(K word) {
-        Node<K, V> myMapNode = searchNode(word);
-        if (myMapNode == null) {
-            return null;
-        } else {
-            return myMapNode.getValue();
-        }
-
-    }
-
-    // Print the linked list
-    @Override
-    public String toString() {
-        return "MyLinkedListNodes{" + head + "} ";
-    }
-}
+	package com.bl.hashTable;
+	
+	import java.util.ArrayList;
+	
+	public class HashTableImplementation<K, V> {
+	    private static final String ArrayofBuckets = null;
+		Node head;
+	    Node tail;
+	    private final int Bucket;
+	    ArrayList<Node<K,V>> ArrayOfBuckets;
+	    
+	    public HashTableImplementation() {
+	        this.Bucket = 10;
+	
+	        // Create empty LinkedLists
+	        for (int i = 0; i < Bucket; i++)
+	            this.ArrayOfBuckets.add(null);
+	    }
+	
+	    public void add(K key, V value) {
+	        Node<K, V> myNode = searchNode(key);
+	        if (myNode == null) {
+	            myNode = new Node<>(key, value);
+	            this.append(myNode);
+	        } else {
+	            myNode.setValue(value);
+	        }
+	    }
+	   
+	
+	    // Append the value in the linked list
+	    public void append(Node myNode) {
+	        if (this.head == null)
+	            this.head = myNode;
+	        if (this.tail == null)
+	            this.tail = myNode;
+	        else {
+	            this.tail.setNext(myNode);
+	            this.tail = myNode;
+	        }
+	    }
+	
+	    // Searching for the word in the linked list
+	    public Node<K, V> searchNode(K data) {
+	        Node currentNode = head;
+	        int position = 0;
+	        while (currentNode != null) {
+	            position++;
+	            if (currentNode.getKey().equals(data)) {
+	                return currentNode;
+	            }
+	            currentNode = currentNode.getNext();
+	        }
+	        return currentNode;
+	    }
+	
+	    // Searching for the word and get the value from the linked list
+	    public V get(K word) {
+	    	int index = this.getBucketIndex(word);
+	        if (this.ArrayOfBuckets.get(index) == null)
+	            return null;
+	        Node<K, V> myMapNode = searchNode(word);
+	        if (myMapNode == null) {
+	            return null;
+	        } else {
+	            return myMapNode.getValue();
+	        }
+	
+	    }
+	   
+	
+	// This implements hash function to find index for a key
+	    public int getBucketIndex(K key) {
+	        int hashCode = Math.abs(key.hashCode());
+	        int index = hashCode % Bucket;
+	        return index;
+	
+	    }
+	
+	    // Print the linked list
+	    @Override
+	    public String toString() {
+	        return "MyLinkedListNodes{" + head + "} ";
+	    }
+	}
